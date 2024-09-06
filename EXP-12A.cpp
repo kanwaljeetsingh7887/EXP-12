695c8c79150acfe75910092827053d7ddb325e8a
Kanwaljeet Singh <97405821+kanwaljeetsingh7887@users.noreply.github.com>
2024-09-06 09:06:54 +0530

    Add files via upload

---
+++ EXP-12A.cpp
@@ -0,0 +1,25 @@
+// NAME - Kanwaljeet singh
+// PRN - 23070123124
+// EXPERIMENT - 12(B)
+
+#include<iostream>
+using namespace std;
+class Num
+{
+    public:
+    Num(int c, int d)
+    {
+        if(c>d)
+        {
+            cout<<c<<" is greater.";
+        }
+        else 
+        {
+            cout<<d<<" is greater.";
+        }
+    }
+};
+int main()
+{
+    Num n1(4,3);
+} 
